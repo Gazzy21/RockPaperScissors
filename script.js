ce5c71333740e931606ad0e9d94d8d6a98db3409
Gazzy21 <nicholas221@icloud.com>
2024-03-15 13:46:48 -0400

    fixed alot of logic

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 document.onload;
 window.onload;
-
+let compImg = document.getElementById("compImg");
+var userImg = document.getElementById("userImg");
+let infoText = document.getElementById("infoText");
 let userPrompt = prompt("Enter your username.");
-document.getElementById("userName").innerText=`${userPrompt}`  
+let compChoice;
 
-infoText = document.getElementById("infoText");
-userImg = document.getElementById("userImg");
-compImg = document.getElementById("compImg");
+document.getElementById("userName").innerText = `${userPrompt}`;
 
 function playRound(userChoice) {
+
+  // set user choice im
+ 
+  userImg.src = `${userChoice}.png`
   //getting comp choice
   let randomNumber = Math.floor(Math.random() * 3);
-  let compChoice;
 
+  console.log(userChoice);
   // setting comp choice to a string value
   if (randomNumber === 0) {
     compChoice = "rock";
-    compImg.innerHTML =
-      '<img src="rock.png" alt="" id="userImg" class="img-fluid compimg shadow-pop-br"/>';
+    compImg.src = "rock.png";
   } else if (randomNumber === 1) {
     compChoice = "paper";
-    compImg.innerHTML =
-      '<img src="paper.png" alt="" id="userImg" class="img-fluid compimg shadow-pop-br"/>';
-  } else if (randomNumber === 2) {
+    compImg.src = "paper.png";
+  } else {
     compChoice = "scissors";
-    compImg.innerHTML =
-      '<img src="scissors.png" alt="" id="userImg" class="img-fluid compimg shadow-pop-br"/>';
+    compImg.src = "scissors.png";
   }
 
   console.log(compChoice);
@@ -34,22 +35,23 @@ function playRound(userChoice) {
   if (userChoice === compChoice) {
     infoText.innerHTML =
       '<h1 class="text-center">Its a TIE!</h1> <p class="text-center">No one has won, no one has lost. No one gets a point, this round did not count. Try again!</p>';
-      document.getElementById
-  } else if (userChoice === "rock" && compChoice === "paper") {
+    
+
+  } else if (userChoice === "rock" && compChoice === "paper" || userChoice === "paper" && compChoice === "scissors" || userChoice === "scissors" && compChoice === "rock") {
     infoText.innerHTML =
       '<h1 class="text-center">THE ROBOTS WIN</h1> <p class="text-center">Bow down to your new overlords!</p>';
+    
+
   } else if (userChoice === "rock" && compChoice === "scissors") {
     infoText.innerHTML =
       '<h1 class="text-center"><span id="userName">User</span> has won!</h1> <p class="text-center">Score one for humanity!</p>';
+
   } else if (userChoice === "paper" && compChoice === "rock") {
     infoText.innerHTML =
       '<h1 class="text-center"><span id="userName">User</span> has won!</h1> <p class="text-center">Score one for humanity!</p>';
-  } else if (userChoice === "paper" && compChoice === "scissors") {
-    infoText.innerHTML =
-      '<h1 class="text-center">THE ROBOTS WIN</h1> <p class="text-center">Bow down to your new overlords!</p>';
-  } else if (userChoice === "scissors" && compChoice === "rock") {
-    infoText.innerHTML =
-      '<h1 class="text-center">THE ROBOTS WIN</h1> <p class="text-center">Bow down to your new overlords!</p>';
+
+  
+
   } else if (userChoice === "scissors" && compChoice === "paper") {
     infoText.innerHTML =
       '<h1 class="text-center"><span id="userName">User</span> has won!</h1> <p class="text-center">Score one for humanity!</p>';
